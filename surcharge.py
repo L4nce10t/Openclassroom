02e29792401d36245fa41a09c9e808a58acc78af
Lancelot <journel@et.esiea.fr>
2022-12-05 13:53:25 +0100

    Je mets ce fichier ici pour separer le code des définition et aussi pour tester

--- surcharge.py
+++ surcharge.py
@@ -1,1 +1,2 @@
 # Methode get() qui fonction avec un dictionnaire, prend en parametre (nomclé , valeur), la valeur est affiché si nomclé existe pas
+# Methode append() qui permet d'ajouter un element dans une liste ou disctionnaire, prend en paramètre list.append(elmnt)
