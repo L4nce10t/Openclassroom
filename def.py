877b2a2d6fe17aec11829c77a9692cfc11b27a9b
Lancelot <journel@et.esiea.fr>
2022-12-05 17:54:03 +0100

    Ajout de nouvelle definition

--- def.py
+++ def.py
@@ -1,2 +1,5 @@
-# Methode get() qui fonction avec un dictionnaire, prend en parametre (nomclé , valeur), la valeur est affiché si nomclé existe pas
-# Methode append() qui permet d'ajouter un element dans une liste ou disctionnaire, prend en paramètre list.append(elmnt)
+# Méthode get() qui fonction avec un dictionnaire, prend en parametre (nomclé , valeur), la valeur est affiché si nomclé existe pas
+# Méthode append() qui permet d'ajouter un element dans une liste ou disctionnaire, prend en paramètre list.append(elmnt)
+# Dans la fonction print l'option f demande a la fonction print de regarder ce qu'il y a dans le variable {}
+# Méthode super() permet d'économiser des lignes de codes en appelant une méthode d'une classe parente
+# Méthode __str__() permet d'indiquer la représentation en chaîne de caractères d'un objet.
